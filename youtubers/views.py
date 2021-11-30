ffeb40becb8b2d816c306a3f7af4287aa161cc11
MarcinKry <73964805+MarcinKry@users.noreply.github.com>
2021-11-30 17:15:47 +0100

    search box fixed

--- youtubers/views.py
+++ youtubers/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Youtuber
 # Create your views here.
 
@@ -9,9 +9,24 @@ def youtubers(request):
     }
     return render(request, 'youtubers/youtubers.html', data)
 
-def youtubers_detail(request):
-    pass
+def youtubers_detail(request, id):
+    tuber = get_object_or_404(Youtuber, pk=id)
+    data = {
+        'tuber': tuber
+    }
+    return render(request, 'youtubers/youtuber_detail.html', data)
 
 def search(request):
-    pass
+    tubers = Youtuber.objects.order_by('-created_date')
+
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            tubers = tubers.filter(description__icontains=keyword)
+
+    
+    data = {
+        'tubers': tubers
+    }
+    return render(request, 'youtubers/search.html', data)
 
