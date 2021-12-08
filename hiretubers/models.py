3c18ad1879e3050a615d1b14527f75d6ecc0491a
MarcinKry <73964805+MarcinKry@users.noreply.github.com>
2021-12-08 20:02:52 +0100

    added contact form to reach out to a specific person

--- hiretubers/models.py
+++ hiretubers/models.py
@@ -1,22 +1,23 @@
 from django.db import models
 from datetime import datetime
 
+from django.db.models.fields import IntegerField
+
 # Create your models here.
 
 
-class Hiretubers(models.Model):
-    first_name = models.CharField(max_length=100),
-    last_name = models.CharField(max_length=100),
-    tuber_id = models.IntegerField(),
-    tuber_name = models.CharField(max_length=100),
-    city = models.CharField(max_length=100),
-    phone = models.CharField(max_length=100),
-    #potentiallly need to remove "","
-    state = models.CharField(max_length=100),
-    message = models.TextField(blank=True),
-    user_id = models.CharField(blank=True),
-    state = models.CharField(max_length=100),
-    created_date = models.DateTimeField(blank=True, default=datetime.now),
+class Hiretuber(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    tuber_id = models.IntegerField()
+    tuber_name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    message = models.TextField(blank=True)
+    user_id = IntegerField(blank=True)
+    created_date = models.DateTimeField(blank=True, default=datetime.now)
 
-    def __self__ (self):
+    def __str__ (self):
         return self.email
